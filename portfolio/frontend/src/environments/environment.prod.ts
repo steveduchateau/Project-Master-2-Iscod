d103ed73e5a9ac4ed7e05b4ad75281fa8997445f
steveduchateau <steveduchateau@outlook.fr>
2024-10-09 16:32:25 +0200

    Mise à jour des urls dans environnements.prod.ts

--- portfolio/frontend/src/environments/environment.prod.ts
+++ portfolio/frontend/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiUrlTechniques: 'https://portfolio-steve-duchateau.onrender.com/api/competences/techniques', // URL de production pour les compétences techniques
-  apiUrlHumaines: 'https://portfolio-steve-duchateau.onrender.com/api/competences/humaines', // URL de production pour les compétences humaines
-  apiUrlProjets: 'https://portfolio-steve-duchateau.onrender.com/api/projets', // URL de production pour les projets
-  apiUrlContact: 'https://portfolio-steve-duchateau.onrender.com/api/contact', // URL de production pour le contact
+  apiUrlTechniques: 'https://project-master-2-iscod.onrender.com/api/competences/techniques', // URL de production pour les compétences techniques
+  apiUrlHumaines: 'https://project-master-2-iscod.onrender.com/api/competences/humaines', // URL de production pour les compétences humaines
+  apiUrlProjets: 'https://project-master-2-iscod.onrender.com/api/projets', // URL de production pour les projets
+  apiUrlContact: 'https://project-master-2-iscod.onrender.com/api/contact', // URL de production pour le contact
   projetUrl: 'https://portfolio-steve-duchateau.onrender.com', // URL pour accéder aux projets en production
   projetUrls: {
     1: 'https://portfolio-steve-duchateau.onrender.com/projets/1', // URL pour le projet 1
