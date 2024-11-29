763e41f5a0103d9e4004ced15f722631ea86591a
steveduchateau <158157951+steveduchateau@users.noreply.github.com>
2024-11-29 09:20:38 +0100

    Update projets.service.ts

--- portfolio/frontend/src/services/projets.service.ts
+++ portfolio/frontend/src/services/projets.service.ts
@@ -1,28 +1,20 @@
-import { Injectable } from '@angular/core'; // Importation du décorateur Injectable pour permettre l'injection de dépendances
-import { HttpClient } from '@angular/common/http'; // Importation de HttpClient pour effectuer des requêtes HTTP
-import { Observable } from 'rxjs'; // Importation de Observable pour la gestion des flux de données asynchrones
+import { Injectable } from '@angular/core'; 
+import { HttpClient } from '@angular/common/http'; 
+import { Observable } from 'rxjs'; 
 
-// Déclaration du service en tant que fournisseur injectable dans toute l'application
 @Injectable({
-  providedIn: 'root' // Spécifie que le service sera disponible dans toute l'application
+  providedIn: 'root'
 })
-// Définition de la classe du service
 export class ProjetsService {
-  // URL de l'API pour accéder aux projets
-  private apiUrl = 'https://project-master-2-iscod.onrender.com/api/projets'; // Nouvelle URL de l'API Flask pour la gestion des projets
+  private apiUrl = 'https://project-master-2-iscod.onrender.com/api/projets'; 
 
-  // Constructeur du service, injecte le client HTTP pour effectuer des requêtes
   constructor(private http: HttpClient) {}
 
-  // Méthode pour obtenir la liste des projets
   getProjets(): Observable<any[]> {
-    // Effectue une requête GET vers l'URL de l'API pour récupérer tous les projets
     return this.http.get<any[]>(this.apiUrl);
   }
 
-  // Méthode pour obtenir les détails d'un projet par son ID
   getProjetById(id: string): Observable<any> {
-    // Effectue une requête GET vers l'URL de l'API pour récupérer un projet spécifique en fonction de son ID
     return this.http.get<any>(`${this.apiUrl}/${id}`);
   }
 }
